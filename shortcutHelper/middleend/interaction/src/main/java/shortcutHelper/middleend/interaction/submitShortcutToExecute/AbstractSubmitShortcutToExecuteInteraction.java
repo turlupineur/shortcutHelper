3ecc8d9941589ef94473dd0a5ea1c5e917d8b562
jsvaillant <jsvaillant@JS_PC.fritz.box>
2019-01-06 01:03:19 +0100

    [str-gui] : Renamed interactions.

--- shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/submitShortcutToExecute/AbstractSubmitShortcutToExecuteInteraction.java
+++ shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/submitShortcutToExecute/AbstractSubmitShortcutToExecuteInteraction.java
@@ -3,7 +3,7 @@ package shortcutHelper.middleend.interaction.submitShortcutToExecute;
 import shortcutHelper.frontend.viewbean.MainInterfaceViewBean;
 import shortcutHelper.middleend.interaction.Interaction;
 
-public abstract class AbstractSubmitShortcutToExecute extends Interaction {
+public abstract class AbstractSubmitShortcutToExecuteInteraction extends Interaction {
 
 
 }
--- shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/submitShortcutToExecute/SubmitShortcutToExecuteInteraction.java
+++ shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/submitShortcutToExecute/SubmitShortcutToExecuteInteraction.java
@@ -2,12 +2,14 @@ package shortcutHelper.middleend.interaction.submitShortcutToExecute;
 
 import shortcutHelper.frontend.viewbean.MainInterfaceViewBean;
 
-public class SubmitShortcutToExecute extends AbstractSubmitShortcutToExecute {
+public class SubmitShortcutToExecuteInteraction extends AbstractSubmitShortcutToExecuteInteraction {
 
 	@Override
 	public void execute(MainInterfaceViewBean bean) {
 		System.out.println("Shortcut to execute: " + bean.getDataBean().getCalledShortcut());
 		bean.getDataBean().setCalledShortcut("");
+		bean.getFunctionBean().setToastMessage("Shortcut executed !");
+		bean.getFunctionBean().setFocusOnShortcutTextBox(true);
 	}
 
 }
