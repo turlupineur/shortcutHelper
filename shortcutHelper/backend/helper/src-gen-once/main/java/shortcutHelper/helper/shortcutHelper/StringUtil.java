be31df7db591fdd8a5dd03350e49131d13e3f627
jsvaillant <jsvaillant@JS_PC.fritz.box>
2019-01-27 23:24:42 +0100

    [str-refresh-function]: Missing file for Helpers.

---
+++ shortcutHelper/backend/helper/src-gen-once/main/java/shortcutHelper/helper/shortcutHelper/StringUtil.java
@@ -0,0 +1,12 @@
+package shortcutHelper.helper.shortcutHelper;
+
+public class StringUtil {
+	public static final String EMPTY_STRING = "";
+	public static final String NULL_STRING = null;
+	
+	
+	public static final boolean isNullOrEmpty(String str)
+	{
+		return str == NULL_STRING || str.length() == 0;
+	}
+}
