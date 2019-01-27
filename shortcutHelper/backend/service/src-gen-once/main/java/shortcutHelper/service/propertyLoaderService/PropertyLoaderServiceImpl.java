31824b6391d0a72439506e23f89d6bd4ede87799
jsvaillant <jsvaillant@JS_PC.fritz.box>
2019-01-27 23:32:56 +0100

    [str-refresh-function]: Modelisation/Implementation of PropertyLoaderService.

---
+++ shortcutHelper/backend/service/src-gen-once/main/java/shortcutHelper/service/propertyLoaderService/PropertyLoaderServiceImpl.java
@@ -0,0 +1,52 @@
+package shortcutHelper.service.propertyLoaderService;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class PropertyLoaderServiceImpl implements IPropertyLoaderService{
+	private Boolean readAsResource;
+	
+	public PropertyLoaderServiceImpl()
+	{
+		this.readAsResource = null;
+		initializeService();
+	}
+
+    public void setReadPropertyAsLocal(){
+    	this.readAsResource = Boolean.FALSE;
+    }
+
+    public void setReadPropertyAsResource(){
+    	this.readAsResource = Boolean.TRUE;
+    }
+
+	@Override
+	public InputStream getInputStreamForFile(String file) throws FileNotFoundException {
+		if(readAsResource.booleanValue())
+		{
+			return this.getClass().getClassLoader().getResourceAsStream(file); 
+		}else {
+			return new FileInputStream(file); 
+		}
+	}
+	
+	private void initializeService()
+	{
+		findPropertyLoaderStrategy();
+	}
+	
+	private void findPropertyLoaderStrategy()
+	{
+		File configFolder = new File("config");
+		if(configFolder.exists())
+		{
+			setReadPropertyAsLocal();
+		}else
+		{
+			setReadPropertyAsResource();
+		}
+	}
+
+}
