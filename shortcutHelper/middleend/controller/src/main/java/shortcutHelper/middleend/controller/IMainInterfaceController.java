938d7af83d4da393b4bb1962a47378c33c8ef2ea
jsvaillant <jsvaillant@JS_PC>
2019-03-04 23:10:26 +0100

    [str-refesh-function]: Forgotten class for Interaction..

--- shortcutHelper/middleend/controller/src/main/java/shortcutHelper/middleend/controller/IMainInterfaceController.java
+++ shortcutHelper/middleend/controller/src/main/java/shortcutHelper/middleend/controller/IMainInterfaceController.java
@@ -3,6 +3,10 @@ package shortcutHelper.middleend.controller;
 import shortcutHelper.frontend.viewbean.MainInterfaceViewBean;
 
 public interface IMainInterfaceController {
+	public static final String BEAN_ID = "MainInterfaceController";
+	/**
+	 * Shows the interface. All needed initialization is performed.
+	 */
 	public void showInterface();
 	
 	/**
