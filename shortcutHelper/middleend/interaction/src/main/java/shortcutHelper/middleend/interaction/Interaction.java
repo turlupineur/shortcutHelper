73fda6d220da36240dda396c08a4585eac2da1b1
jsvaillant <jsvaillant@JS_PC>
2019-03-04 23:10:55 +0100

    [str-refesh-function]: Forgotten class for Interaction.

--- shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/Interaction.java
+++ shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/Interaction.java
@@ -1,7 +1,4 @@
 package shortcutHelper.middleend.interaction;
 
-import shortcutHelper.frontend.viewbean.MainInterfaceViewBean;
-
-public abstract class Interaction {
-	public abstract void execute(MainInterfaceViewBean bean);
+public abstract class Interaction implements IInteraction{
 }
