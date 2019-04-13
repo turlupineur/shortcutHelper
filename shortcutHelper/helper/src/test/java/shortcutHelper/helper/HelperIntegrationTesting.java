e3252e10dbb246584afec0df7baf86e8db330ffc
jsVaillant <jean.simon.vaillant@gmail.com>
2019-04-14 01:26:49 +0200

    [restructurationCode] Restructuration of beans for modules util, helper and service

--- shortcutHelper/helper/src/test/java/shortcutHelper/helper/HelperIntegrationTesting.java
+++ shortcutHelper/helper/src/test/java/shortcutHelper/helper/HelperIntegrationTesting.java
@@ -6,9 +6,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/config/beans/backend/util/util.xml", "/config/beans/backend/helper/helper.xml",
-		"/config/beans/backend/util/util-man.xml", "/config/beans/backend/helper/helper-man.xml",
-		"/config/beans/backend/service/service-man.xml", "/config/beans/backend/service/service.xml" })
+@ContextConfiguration(locations = { "/config/beans/util/util.xml", "/config/beans/helper/helper.xml",
+		"/config/beans/util/util-man.xml", "/config/beans/helper/helper-man.xml",
+		"/config/beans/service/service-man.xml", "/config/beans/service/service.xml" })
 public class HelperIntegrationTesting {
 	@Before
 	public void setUp() {
--- shortcutHelper/util/src/test/java/shortcutHelper/util/UtilIntegrationTesting.java
+++ shortcutHelper/util/src/test/java/shortcutHelper/util/UtilIntegrationTesting.java
@@ -6,12 +6,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/config/beans/backend/util/util.xml", "/config/beans/backend/util/util-man.xml" })
+@ContextConfiguration(locations = { "/config/beans/util/util.xml", "/config/beans/util/util-man.xml" })
 public class UtilIntegrationTesting {
 	@Before
-	public void setUp()
-	{
-		
+	public void setUp() {
+
 	}
-	
+
 }
