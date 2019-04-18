097e811f57651930c804eb9887e8618756eae660
jsVaillant <jean.simon.vaillant@gmail.com>
2019-04-18 22:38:21 +0200

    [str-clipboard] Added base for tests in service.

---
+++ shortcutHelper/service/src/test/java/shortcutHelper/service/ServiceTesting.java
@@ -0,0 +1,13 @@
+package shortcutHelper.service;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class ServiceTesting {
+	@Before
+	public void setUp() {
+
+	}
+}
