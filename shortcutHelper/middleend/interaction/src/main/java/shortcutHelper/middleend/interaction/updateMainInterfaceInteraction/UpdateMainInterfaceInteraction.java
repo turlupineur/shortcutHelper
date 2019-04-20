0337d901fc7a96e08aab722fd5f586d07b21b477
jsVaillant <jean.simon.vaillant@gmail.com>
2019-04-20 15:50:12 +0200

    [str-copyToClipboardFunctionality] Fix bug display of shortcuts.

--- shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/updateMainInterfaceInteraction/UpdateMainInterfaceInteraction.java
+++ shortcutHelper/middleend/interaction/src/main/java/shortcutHelper/middleend/interaction/updateMainInterfaceInteraction/UpdateMainInterfaceInteraction.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import shortcutHelper.frontend.viewbean.MainInterfaceViewBean;
 
-public class UpdateMainInterfaceInteraction extends AbstractUpdateMainInterfaceInteraction{
+public class UpdateMainInterfaceInteraction extends AbstractUpdateMainInterfaceInteraction {
 
 	@Override
 	public void execute(MainInterfaceViewBean bean) {
 		getUpdateMainInterfaceBackendInteraction().execute(null);
 		List<String> listShortcutsToShow = new ArrayList<String>();
-		listShortcutsToShow.addAll(this.getShortcutHelper().getListShortcuts().values());
+		listShortcutsToShow.addAll(this.getShortcutHelper().getListShortcuts().keySet());
 		bean.getDataBean().setShortcuts(listShortcutsToShow);
 	}
 
