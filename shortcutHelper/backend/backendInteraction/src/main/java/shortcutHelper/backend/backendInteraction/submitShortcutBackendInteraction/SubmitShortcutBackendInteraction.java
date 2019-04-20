5c4a442b0dc77e04ec02b31306b443dcfef00b2f
jsVaillant <jean.simon.vaillant@gmail.com>
2019-04-20 15:52:45 +0200

    [str-copyToClipboardFunctionality] Fix call of Functionality.

--- shortcutHelper/backend/backendInteraction/src/main/java/shortcutHelper/backend/backendInteraction/submitShortcutBackendInteraction/SubmitShortcutBackendInteraction.java
+++ shortcutHelper/backend/backendInteraction/src/main/java/shortcutHelper/backend/backendInteraction/submitShortcutBackendInteraction/SubmitShortcutBackendInteraction.java
@@ -1,14 +1,25 @@
 package shortcutHelper.backend.backendInteraction.submitShortcutBackendInteraction;
 
 import shortcutHelper.backend.backendInteraction.BackendInteractionDataContainer;
+import shortcutHelper.backend.functionality.executerFunctionality.ExecuterFunctionalityDataContainer;
+import shortcutHelper.backendCommon.ShortcutHelperContext;
+import shortcutHelper.helper.shortcutFactoryHelper.IShortcut;
 
-public class SubmitShortcutBackendInteraction extends AbstractSubmitShortcutBackendInteraction{
+public class SubmitShortcutBackendInteraction extends AbstractSubmitShortcutBackendInteraction {
 
 	@Override
 	public void execute(BackendInteractionDataContainer c) {
 		SubmitShortcutBackendInteractionDataContainer container = (SubmitShortcutBackendInteractionDataContainer) c;
 		// TODO Auto-generated method stub
 		System.out.println("BackendInteraction shortcut: " + container.getShortcutToExecute());
+
+		String shortcutRaw = this.getShortcutHelper().getShortcut(container.getShortcutToExecute());
+		IShortcut shortcut = this.getShortcutFactoryHelper().parseShortcut(shortcutRaw);
+
+		ExecuterFunctionalityDataContainer containerCmd = new ExecuterFunctionalityDataContainer();
+		containerCmd.setShortcutHelperContext(new ShortcutHelperContext());
+		containerCmd.setShortcutToExecute(shortcut);
+		this.getExecuterFunctionality().run(containerCmd);
 	}
 
 }
