cd5de96a59c9d7123a2841e29a9c65c1de5c15ea
jsVaillant <jean.simon.vaillant@gmail.com>
2019-04-20 16:00:38 +0200

    [str-copyToClipboardFunctionality] Added required beans to run application.

--- shortcutHelper/main/src/main/java/shortcutHelper/main/ShortcutHelperMain.java
+++ shortcutHelper/main/src/main/java/shortcutHelper/main/ShortcutHelperMain.java
@@ -1,38 +1,34 @@
 package shortcutHelper.main;
 
-import java.net.URL;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import shortcutHelper.helper.Helper;
-import shortcutHelper.helper.propertyHelper.IPropertyHelper;
 import shortcutHelper.middleend.controller.IMainInterfaceController;
-import shortcutHelper.middleend.controller.MainInterfaceController;
 
 public class ShortcutHelperMain {
 
-	
 	public static void main(String[] args) {
-//		URL url = ShortcutHelperMain.class.getClass().getResource("/config/beans/backend/backendcommand/backendCommand-data-container-creator.xml");
-//		System.out.println(url);
+		// URL url =
+		// ShortcutHelperMain.class.getClass().getResource("/config/beans/backend/backendcommand/backendCommand-data-container-creator.xml");
+		// System.out.println(url);
 		ApplicationContext context = loadBeans();
-		IMainInterfaceController controller = (IMainInterfaceController) context.getBean(IMainInterfaceController.BEAN_ID);
+		IMainInterfaceController controller = (IMainInterfaceController) context
+				.getBean(IMainInterfaceController.BEAN_ID);
 		controller.showInterface();
 	}
-	private static ApplicationContext loadBeans()
-	{
+
+	private static ApplicationContext loadBeans() {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
-				"/config/beans/backend/backendcommand/backendCommand-data-container-creator-man.xml",
-				"/config/beans/backend/backendcommand/backendCommand-data-container-data-extractor-man.xml",
-				"/config/beans/backend/backendcommand/backendCommand-man.xml",
-				"/config/beans/backend/helper/helper-man.xml",
-				"/config/beans/backend/service/service-man.xml",
+				"/config/beans/backend/functionality/functionality-data-container-creator-man.xml",
+				"/config/beans/backend/functionality/functionality-data-container-data-extractor-man.xml",
+				"/config/beans/backend/functionality/functionality-man.xml", "/config/beans/helper/helper-man.xml",
+				"/config/beans/util/util-man.xml", "/config/beans/service/service-man.xml",
 				"/config/beans/backend/backendinteraction/backendInteraction.xml",
-				"/config/beans/middleend/interaction/interaction.xml",
-				"/config/beans/middleend/controller/controller.xml",
-				"/config/beans/frontend/interface/interface.xml"
-				);
+				"/config/beans/middleend/interaction/interaction.xml", "/config/beans/logic/logic-man.xml",
+				"/config/beans/middleend/controller/controller.xml", "/config/beans/frontend/interface/interface.xml",
+				"/config/beans/backend/functionality/functionality_command.xml",
+				"/config/beans/logic/logic-data-container-creator-man.xml",
+				"/config/beans/backend/functionality/functionality-man.xml");
 		return context;
 	}
 }
