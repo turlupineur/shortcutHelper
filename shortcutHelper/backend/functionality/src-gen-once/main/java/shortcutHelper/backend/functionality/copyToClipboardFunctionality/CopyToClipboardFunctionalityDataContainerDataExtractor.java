60cc3fd9addf20d6f4b118c4ceb777d0ce189524
jsVaillant <jean.simon.vaillant@gmail.com>
2021-03-13 22:10:25 +0100

    Fix problem with copy to clipboard.

--- shortcutHelper/backend/functionality/src-gen-once/main/java/shortcutHelper/backend/functionality/copyToClipboardFunctionality/CopyToClipboardFunctionalityDataContainerDataExtractor.java
+++ shortcutHelper/backend/functionality/src-gen-once/main/java/shortcutHelper/backend/functionality/copyToClipboardFunctionality/CopyToClipboardFunctionalityDataContainerDataExtractor.java
@@ -2,9 +2,10 @@ package shortcutHelper.backend.functionality.copyToClipboardFunctionality;
 
 import shortcutHelper.backend.functionality.DataContainer;
 
-public class CopyToClipboardFunctionalityDataContainerDataExtractor extends AbstractCopyToClipboardFunctionalityDataContainerDataExtractor{
-	public void extractData(String [] rawData, DataContainer c)
-	{
-		// implement here
+public class CopyToClipboardFunctionalityDataContainerDataExtractor
+		extends AbstractCopyToClipboardFunctionalityDataContainerDataExtractor {
+	public void extractData(String[] rawData, DataContainer c) {
+		CopyToClipboardFunctionalityDataContainer container = (CopyToClipboardFunctionalityDataContainer) c;
+		container.setClipboard(rawData[0]);
 	}
 }
--- shortcutHelper/backend/functionality/src-gen-once/main/java/shortcutHelper/backend/functionality/copyToClipboardFunctionality/CopyToClipboardFunctionalityImpl.java
+++ shortcutHelper/backend/functionality/src-gen-once/main/java/shortcutHelper/backend/functionality/copyToClipboardFunctionality/CopyToClipboardFunctionalityImpl.java
@@ -1,14 +1,18 @@
 package shortcutHelper.backend.functionality.copyToClipboardFunctionality;
 
-import shortcutHelper.backend.functionality.FunctionalityResult;
+import shortcutHelper.backend.functionality.ConcreteFunctionalityResult;
 import shortcutHelper.backend.functionality.DataContainer;
+import shortcutHelper.backend.functionality.FunctionalityResult;
+import shortcutHelper.backend.logic.behavior.ClipboardSetBehavior;
 
 public class CopyToClipboardFunctionalityImpl extends AbstractCopyToClipboardFunctionality
-{
-	public FunctionalityResult runImpl(DataContainer c)
-	{
-		CopyToClipboardFunctionalityDataContainer container = (CopyToClipboardFunctionalityDataContainer ) c; 
-		// own implementation here
-		return null;
+		implements ClipboardSetBehavior {
+	public FunctionalityResult runImpl(DataContainer c) {
+		CopyToClipboardFunctionalityDataContainer container = (CopyToClipboardFunctionalityDataContainer) c;
+
+		setClipboard(container.getClipboard());
+
+		container.getShortcutHelperContext().setInfo("Value copied to clipboard !");
+		return ConcreteFunctionalityResult.RESULT_OK;
 	}
-} 
+}
