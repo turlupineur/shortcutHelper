6b9c3ad1d8426a92272e1b2b84ac4ef314ebf45f
jsVaillant <jean.simon.vaillant@gmail.com>
2021-04-11 12:03:01 +0200

    Fix errorprone path.

--- shortcutHelper/common/src/main/java/shortcutHelper/common/properties/data/SetStorageOnTheFlyFunctionalityProperties.java
+++ shortcutHelper/common/src/main/java/shortcutHelper/common/properties/data/SetStorageOnTheFlyFunctionalityProperties.java
@@ -8,6 +8,11 @@ public class SetStorageOnTheFlyFunctionalityProperties {
 	}
 
 	public void setStorageFolder(String storageFolder) {
+		// present invalid path not ending with path separator.
+		if (storageFolder.length() != 0
+				&& storageFolder.lastIndexOf(java.io.File.separator) != storageFolder.length() - 1) {
+			storageFolder = storageFolder + java.io.File.separator;
+		}
 		this.storageFolder = storageFolder;
 	}
 }
